bdd37af5f271a63efb19e74227837309c6d21596
kevin.velasco <kevin.velasco@gmail.com>
2020-06-09 21:31:08 -0700

    Adding stub test class

---
+++ coroutines/src/test/kotlin/Stub.kt
@@ -0,0 +1,10 @@
+import org.junit.Test
+
+class Stub {
+
+    @Test
+    fun stub() {
+        // Stub to trigger Jacoco coverage reports.
+        // Should be removed when other tests are added to this module.
+    }
+}
