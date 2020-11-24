cf2116976b734ff9811d75e192c6346e7456428d
Cedrick Cooke <development@cedrickc.net>
2020-11-24 14:14:56 -0800

    Codecov fix and repeat requirement message update

--- functional/src/commonMain/kotlin/Repeat.kt
+++ functional/src/commonMain/kotlin/Repeat.kt
@@ -2,7 +2,7 @@ package com.juul.tuulbox.functional
 
 /** Returns a function that repeats the application of [this] [count] times. */
 public fun <T> ((T) -> T).repeat(count: Int): (T) -> T {
-    require(count >= 0) { "`times` must not be negative" }
+    require(count >= 0) { "Repeat `count` must not be negative." }
     return { initialValue ->
         var value = initialValue
         kotlin.repeat(count) {
