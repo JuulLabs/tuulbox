30b3de9dd5330ae8ded62b5b2c44a84771b5809e
Cedrick Cooke <development@cedrickc.net>
2020-12-02 16:04:24 -0800

    Fix lint failure

--- logging/src/commonMain/kotlin/Log.kt
+++ logging/src/commonMain/kotlin/Log.kt
@@ -17,35 +17,35 @@ public object Log {
     }
 
     /** Send a debug-level log message to the global dispatcher. */
-    public fun debug(throwable: Throwable? = null, tag: String? = null,  message: () -> String) {
+    public fun debug(throwable: Throwable? = null, tag: String? = null, message: () -> String) {
         if (dispatcher.hasConsumers) {
             dispatcher.debug(tag ?: tagGenerator.getTag(), message.invoke(), throwable)
         }
     }
 
     /** Send an info-level log message to the global dispatcher. */
-    public fun info(throwable: Throwable? = null, tag: String? = null,  message: () -> String) {
+    public fun info(throwable: Throwable? = null, tag: String? = null, message: () -> String) {
         if (dispatcher.hasConsumers) {
             dispatcher.info(tag ?: tagGenerator.getTag(), message.invoke(), throwable)
         }
     }
 
     /** Send an warn-level log message to the global dispatcher. */
-    public fun warn(throwable: Throwable? = null, tag: String? = null,  message: () -> String) {
+    public fun warn(throwable: Throwable? = null, tag: String? = null, message: () -> String) {
         if (dispatcher.hasConsumers) {
             dispatcher.warn(tag ?: tagGenerator.getTag(), message.invoke(), throwable)
         }
     }
 
     /** Send an error-level log message to the global dispatcher. */
-    public fun error(throwable: Throwable? = null, tag: String? = null,  message: () -> String) {
+    public fun error(throwable: Throwable? = null, tag: String? = null, message: () -> String) {
         if (dispatcher.hasConsumers) {
             dispatcher.error(tag ?: tagGenerator.getTag(), message.invoke(), throwable)
         }
     }
 
     /** Send an assert-level log message to the global dispatcher. */
-    public fun assert(throwable: Throwable? = null, tag: String? = null,  message: () -> String) {
+    public fun assert(throwable: Throwable? = null, tag: String? = null, message: () -> String) {
         if (dispatcher.hasConsumers) {
             dispatcher.assert(tag ?: tagGenerator.getTag(), message.invoke(), throwable)
         }
