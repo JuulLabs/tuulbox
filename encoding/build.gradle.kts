a932f7b517af2e95970c21cd1b645b2f82e6e5ca
Cedrick Cooke <cedrick.cooke@juul.com>
2021-03-15 12:50:03 -0700

    Add 'encoding' module with BitSet support

---
+++ encoding/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("multiplatform")
+    id("org.jmailen.kotlinter")
+    jacoco
+    id("org.jetbrains.dokka")
+    id("com.vanniktech.maven.publish")
+}
+
+apply(from = rootProject.file("gradle/jacoco.gradle.kts"))
+apply(from = rootProject.file("gradle/publish.gradle.kts"))
+
+kotlin {
+    explicitApi()
+
+    jvm()
+    js().browser()
+    macosX64()
+
+    sourceSets {
+        val commonTest by getting {
+            dependencies {
+                implementation(project(":test"))
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
