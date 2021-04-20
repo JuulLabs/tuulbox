274763b0944ef904284e97c6fd8b374abe17b38b
Cedrick Cooke <development@cedrickc.net>
2021-04-20 14:00:15 -0700

    Update publishing plugin version (#54)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("org.jmailen.kotlinter") version "3.2.0" apply false
     id("binary-compatibility-validator") version "0.2.3"
     id("org.jetbrains.dokka") version "1.4.30"
-    id("com.vanniktech.maven.publish") version "0.14.0" apply false
+    id("com.vanniktech.maven.publish") version "0.14.2" apply false
     id("net.mbonnin.one.eight") version "0.1"
 }
 
