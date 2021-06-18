94895ce4b3a336ddac9f026f03f75ccc866bca62
Travis Wyatt <travis@juul.com>
2021-06-18 02:01:17 -0700

    Add iOS targets to `test` module and bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,31 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-
 buildscript {
     repositories {
         google()
-        jcenter()
+        mavenCentral()
     }
 }
 
 plugins {
-    kotlin("multiplatform") version "1.4.31" apply false
-    id("com.android.library") version "4.0.2" apply false
-    id("kotlinx-atomicfu") version "0.15.1" apply false
-    id("org.jmailen.kotlinter") version "3.2.0" apply false
+    kotlin("multiplatform") version "1.5.10" apply false
+    id("com.android.library") version "4.1.3" apply false
+    id("kotlinx-atomicfu") version "0.16.1" apply false
+    id("org.jmailen.kotlinter") version "3.4.4" apply false
     id("binary-compatibility-validator") version "0.2.3"
-    id("org.jetbrains.dokka") version "1.4.30"
-    id("com.vanniktech.maven.publish") version "0.14.2" apply false
-    id("net.mbonnin.one.eight") version "0.1"
+    id("org.jetbrains.dokka") version "1.4.32"
+    id("com.vanniktech.maven.publish") version "0.15.1" apply false
+    id("net.mbonnin.one.eight") version "0.2"
 }
 
 allprojects {
     repositories {
         google()
-        jcenter()
-        maven("https://kotlin.bintray.com/kotlinx/")
+        mavenCentral()
     }
 
     tasks.withType<Test>().configureEach {
         testLogging {
             events("started", "passed", "skipped", "failed", "standardOut", "standardError")
-            exceptionFormat = FULL
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
             showExceptions = true
             showStackTraces = true
             showCauses = true
