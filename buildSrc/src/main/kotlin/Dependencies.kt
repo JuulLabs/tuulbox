0a4d7c5645c00952d3beaf4dcb20dbcd1cc4ebcf
Travis Wyatt <travis@juul.com>
2021-06-18 12:52:37 -0700

    Bump Coroutines and datetime versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,11 +2,11 @@ object kotlinx {
 
     fun coroutines(
         module: String = "core",
-        version: String = "1.4.0"
+        version: String = "1.5.0"
     ) = "org.jetbrains.kotlinx:kotlinx-coroutines-$module:$version"
 
     fun datetime(
-        version: String = "0.1.1"
+        version: String = "0.2.1"
     ) = "org.jetbrains.kotlinx:kotlinx-datetime:$version"
 }
 
