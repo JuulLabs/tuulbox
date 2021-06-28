fde4eb74d2aeb37b6aaac2002bccc553adc02c5a
KVelasco <kevin.velasco@gmail.com>
2021-06-28 13:09:30 -0700

    JavaScript IR Support (#66)
    * JavaScript IR Support
    
    * JavaScript IR Support
    
    Co-authored-by: Travis Wyatt <travis@juul.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,15 @@ plugins {
     id("com.android.library") version "4.1.3" apply false
     id("kotlinx-atomicfu") version "0.16.1" apply false
     id("org.jmailen.kotlinter") version "3.4.4" apply false
-    id("binary-compatibility-validator") version "0.2.3"
     id("org.jetbrains.dokka") version "1.4.32"
     id("com.vanniktech.maven.publish") version "0.15.1" apply false
     id("net.mbonnin.one.eight") version "0.2"
+    // Breaks with:
+    // A problem occurred configuring project ':registration'.
+    // > org.gradle.api.internal.tasks.DefaultTaskContainer$DuplicateTaskException: Cannot add task 'apiBuild' as a task with that name already exists.
+    //
+    // Disabling until https://github.com/Kotlin/binary-compatibility-validator/issues/38 is fixed.
+    //    id("binary-compatibility-validator") version "0.6.0"
 }
 
 allprojects {
--- temporal/src/jsMain/kotlin/TickerTemporalFlow.kt
+++ temporal/src/jsMain/kotlin/TickerTemporalFlow.kt
@@ -1,12 +1,15 @@
 package com.juul.tuulbox.temporal
 
 import kotlinx.coroutines.flow.Flow
+import kotlin.time.ExperimentalTime
 import kotlin.time.minutes
 
+@ExperimentalTime
 internal actual inline fun <T> inlineTemporalFlow(
     crossinline factory: () -> T
 ): Flow<T> = ticker(1.minutes, factory)
 
+@ExperimentalTime
 public actual fun <T> temporalFlow(
     factory: () -> T
 ): Flow<T> = ticker(1.minutes, factory)
