97df3c7141effdf8636755acfaebcf8377ff5c0b
Travis Wyatt <travis@juul.com>
2021-07-03 00:12:19 -0700

    Replace deprecated `offer` with `trySend`

--- coroutines/src/androidMain/kotlin/flow/BroadcastReceiverFlow.kt
+++ coroutines/src/androidMain/kotlin/flow/BroadcastReceiverFlow.kt
@@ -4,10 +4,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.util.Log
 import com.juul.tuulbox.coroutines.applicationContext
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+
+private const val TAG = "Tuulbox/coroutines/broadcastReceiverFlow"
 
 @Suppress("EXPERIMENTAL_API_USAGE")
 public fun broadcastReceiverFlow(
@@ -16,7 +19,7 @@ public fun broadcastReceiverFlow(
     val broadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             if (intent != null) {
-                offer(intent)
+                if (!trySend(intent).isSuccess) Log.w(TAG, "Failed to deliver intent.")
             }
         }
     }
