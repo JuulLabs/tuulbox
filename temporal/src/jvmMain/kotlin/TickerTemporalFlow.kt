8373a3f22f6402241911c54efa6c2057734588a0
Travis Wyatt <travis@juul.com>
2021-07-06 12:00:09 -0700

    Change duration usages to fix deprecation warnings (#69)

--- temporal/src/jvmMain/kotlin/TickerTemporalFlow.kt
+++ temporal/src/jvmMain/kotlin/TickerTemporalFlow.kt
@@ -1,12 +1,12 @@
 package com.juul.tuulbox.temporal
 
 import kotlinx.coroutines.flow.Flow
-import kotlin.time.minutes
+import kotlin.time.Duration
 
 internal actual inline fun <T> inlineTemporalFlow(
     crossinline factory: () -> T
-): Flow<T> = ticker(1.minutes, factory)
+): Flow<T> = ticker(Duration.minutes(1), factory)
 
 public actual fun <T> temporalFlow(
     factory: () -> T
-): Flow<T> = ticker(1.minutes, factory)
+): Flow<T> = ticker(Duration.minutes(1), factory)
