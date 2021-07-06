146a5ec630aa4c7c3b729bd86beeac50cb50b705
Travis Wyatt <travis@juul.com>
2021-07-06 11:41:04 -0700

    Remove unused import

--- coroutines/src/androidMain/kotlin/flow/BroadcastReceiverFlow.kt
+++ coroutines/src/androidMain/kotlin/flow/BroadcastReceiverFlow.kt
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.util.Log
 import com.juul.tuulbox.coroutines.applicationContext
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
