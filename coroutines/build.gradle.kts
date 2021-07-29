d0567931dfc6a3f5399153211d3ec3eb657ae7bf
Cedrick Cooke <development@cedrickc.net>
2021-07-29 11:20:39 -0700

    Bump unrelated Android target SDK because Gradle loves breaking builds

--- coroutines/build.gradle.kts
+++ coroutines/build.gradle.kts
@@ -84,11 +84,11 @@ kotlin {
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdkVersion(31)
 
     defaultConfig {
         minSdkVersion(24)
-        targetSdkVersion(30)
+        targetSdkVersion(31)
     }
 
     lintOptions {
