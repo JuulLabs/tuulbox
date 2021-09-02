1613a41a2e932c538fdea155f9b8982f727b6d45
Cedrick Cooke <development@cedrickc.net>
2021-09-02 13:34:12 -0700

    Add `Sensitivity` metadata

---
+++ logging/src/commonMain/kotlin/Sensitivity.kt
@@ -0,0 +1,14 @@
+package com.juul.tuulbox.logging
+
+/**
+ * Standardized metadata for data sensitivity levels.
+ *
+ * - [NotSensitive]: Marks a log as definitely free of PII or other sensitive data.
+ * - [Sensitive]: Marks a log as definitely containing PII or other sensitive data.
+ */
+public enum class Sensitivity {
+    NotSensitive,
+    Sensitive;
+
+    public companion object : Key<Sensitivity>
+}
