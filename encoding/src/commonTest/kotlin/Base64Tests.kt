41dd13094f61b2fb73d06d89f7d0a494464f5523
Cedrick Cooke <development@cedrickc.net>
2021-10-20 11:09:23 -0700

    Base 64 Encoder & Decoder (#91)

---
+++ encoding/src/commonTest/kotlin/Base64Tests.kt
@@ -0,0 +1,61 @@
+package com.juul.tuulbox.encoding
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/** See: https://www.rfc-editor.org/rfc/rfc4648#section-10 */
+private val testVectors = listOf(
+    "" to "",
+    "f" to "Zg==",
+    "fo" to "Zm8=",
+    "foo" to "Zm9v",
+    "foob" to "Zm9vYg==",
+    "fooba" to "Zm9vYmE=",
+    "foobar" to "Zm9vYmFy"
+)
+
+private val omittedPaddingTestVectors = listOf(
+    "f" to "Zg",
+    "fo" to "Zm8",
+    "foob" to "Zm9vYg",
+    "fooba" to "Zm9vYmE",
+)
+
+class Base64Tests {
+
+    @Test
+    fun decodeBase64_forRfcTestVectors_matchesRfc() {
+        for ((expected, base64) in testVectors) {
+            assertEquals(expected, base64.decodeBase64().decodeToString())
+        }
+    }
+
+    @Test
+    fun decodeBase64_withOmittedPadding_isRecoverable() {
+        for ((expected, base64) in omittedPaddingTestVectors) {
+            assertEquals(expected, base64.decodeBase64().decodeToString())
+        }
+    }
+
+    @Test
+    fun decodeBase64Sequence_forRfcTestVectors_matchesRfc() {
+        for ((expected, base64) in testVectors) {
+            assertEquals(expected, base64.decodeBase64Sequence().toList().toByteArray().decodeToString())
+        }
+    }
+
+    @Test
+    fun decodeBase64Sequence_withOmittedPadding_isRecoverable() {
+        for ((expected, base64) in omittedPaddingTestVectors) {
+            assertEquals(expected, base64.decodeBase64Sequence().toList().toByteArray().decodeToString())
+        }
+    }
+
+    @Test
+    fun encodeBase64_forRfcTestVectors_matchesRfc() {
+        for ((input, expected) in testVectors) {
+            println(input)
+            assertEquals(expected, input.encodeToByteArray().encodeBase64())
+        }
+    }
+}
