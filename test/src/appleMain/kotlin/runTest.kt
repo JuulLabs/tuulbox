5404afafe672d738c1efbff0bc73834c96dfa9a5
Travis Wyatt <travis@juul.com>
2021-12-31 19:15:04 -0800

    Deprecate `runTest`

--- test/src/appleMain/kotlin/runTest.kt
+++ test/src/appleMain/kotlin/runTest.kt
@@ -4,6 +4,7 @@ import kotlinx.cinterop.memScoped
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 
+@Deprecated("Use official Coroutines `runTest`")
 public actual fun runTest(
     action: suspend CoroutineScope.() -> Unit
 ): Unit = runBlocking {
