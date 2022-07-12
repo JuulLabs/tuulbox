467ca80cf6f2b7b9374ee9a1f848014b7870ab97
David Taylor <51968233+davidtaylor-juul@users.noreply.github.com>
2022-07-12 11:55:13 -0700

    Added BitSet Extract and Mask functions (#196)

--- encoding/src/commonMain/kotlin/BitSet.kt
+++ encoding/src/commonMain/kotlin/BitSet.kt
@@ -4,7 +4,14 @@ public interface BitSet<T> {
     public operator fun get(index: Int): Boolean
     public operator fun set(index: Int, value: Boolean): BitSet<T>
     public fun asPrimitive(): T
+
+    /** Extracts [count] bits at [offset]. e.g. `0b00001100.extract(2, 2) = 0b11 = 0x03`. */
+    public fun extract(offset: Int, count: Int): T
 }
+
+/** Extracts [range] bits. e.g. `0b00001100.extract(2..3) = 0b11 = 0x03`. */
+public fun <T> BitSet<T>.extract(range: IntRange): T =
+    extract(range.first, range.last - range.first + 1)
 
 // Would kill for C++ templates right about now. Which is not something I can normally say.
 
@@ -27,6 +34,12 @@ public data class IntBitSet(private var buffer: Int) : BitSet<Int> {
     }
 
     override fun asPrimitive(): Int = buffer
+
+    override fun extract(offset: Int, count: Int): Int {
+        require(offset in 0 until 32) { "Offset expected 0-31 but got $offset" }
+        require(count in 0 until 32 - offset + 1) { "count expected 0-${32 - offset + 1} but got $count" }
+        return (buffer ushr offset) and flatBitMask(count)
+    }
 }
 
 public data class LongBitSet(private var buffer: Long) : BitSet<Long> {
@@ -45,4 +58,22 @@ public data class LongBitSet(private var buffer: Long) : BitSet<Long> {
     }
 
     override fun asPrimitive(): Long = buffer
+
+    override fun extract(offset: Int, count: Int): Long {
+        require(offset in 0 until 64) { "Offset expected 0-63 but got $offset" }
+        require(count in 0 until 64 - offset + 1) { "count expected 0-${64 - offset + 1} but got $count" }
+        return (buffer ushr offset) and flatLongBitMask(count)
+    }
+}
+
+/** Create an [Int] mask of `count` length e.g. flatBitMask(3) = 0b00000111`. */
+public fun flatBitMask(count: Int): Int {
+    require(count in 0..32) { "count expected 0-32 but got $count" }
+    return if (count == 32) -1 else (1 shl count) - 1
+}
+
+/** Create a [Long] mask of `count` length e.g. flatLongBitMask(3) = 0b00000111L`. */
+public fun flatLongBitMask(count: Int): Long {
+    require(count in 0..64) { "count expected 0-64 but got $count" }
+    return if (count == 64) -1L else (1L shl count) - 1L
 }
