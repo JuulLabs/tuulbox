8e7c86d67b8deb9925b9ce26c0da41b599817fdc
David Taylor <david.taylor@juul.com>
2022-08-10 09:33:47 -0700

    Removed default time

--- test/src/commonMain/kotlin/ManualClock.kt
+++ test/src/commonMain/kotlin/ManualClock.kt
@@ -6,16 +6,8 @@ import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toInstant
 
-private val defaultTime: Instant = LocalDateTime(
-    year = 2022,
-    monthNumber = 3,
-    dayOfMonth = 8,
-    hour = 8,
-    minute = 15,
-).toInstant(TimeZone.UTC)
-
 public class ManualClock(
-    private var time: Instant = defaultTime
+    private var time: Instant,
 ) : Clock {
 
     public constructor(time: LocalDateTime, timeZone: TimeZone) : this(time.toInstant(timeZone))
