dd262f01a07e8fbed0471965881f15af4065ace6
Travis Wyatt <travis@juul.com>
2023-09-07 23:07:28 -0700

    Drop support for deprecated `iosArm32` target

--- coroutines/build.gradle.kts
+++ coroutines/build.gradle.kts
@@ -19,7 +19,6 @@ kotlin {
     macosX64()
     macosArm64()
     iosX64()
-    iosArm32()
     iosArm64()
     iosSimulatorArm64()
 
@@ -77,10 +76,6 @@ kotlin {
         }
 
         val iosX64Main by getting {
-            dependsOn(appleMain)
-        }
-
-        val iosArm32Main by getting {
             dependsOn(appleMain)
         }
 
