482b3d5e23f90ee038809fa1f189fab91f6c5f86
Cedrick Cooke <development@cedrickc.net>
2025-11-17 16:24:42 -0800

    Add support for wasmJs and take advantage of default hierarchy (#508)

--- functional/build.gradle.kts
+++ functional/build.gradle.kts
@@ -19,6 +19,9 @@ kotlin {
     iosX64()
     iosArm64()
     iosSimulatorArm64()
+    wasmJs().browser()
+
+    applyDefaultHierarchyTemplate()
 
     sourceSets {
         val commonMain by getting
@@ -26,45 +29,8 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(libs.kotlinx.coroutines.test)
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
+                implementation(kotlin("test"))
             }
-        }
-
-        val jvmTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit"))
-            }
-        }
-
-        val jsTest by getting {
-            dependencies {
-                implementation(kotlin("test-js"))
-            }
-        }
-
-        val appleMain by creating {
-            dependsOn(commonMain)
-        }
-
-        val macosX64Main by getting {
-            dependsOn(appleMain)
-        }
-
-        val macosArm64Main by getting {
-            dependsOn(appleMain)
-        }
-
-        val iosX64Main by getting {
-            dependsOn(appleMain)
-        }
-
-        val iosArm64Main by getting {
-            dependsOn(appleMain)
-        }
-
-        val iosSimulatorArm64Main by getting {
-            dependsOn(appleMain)
         }
     }
 }
